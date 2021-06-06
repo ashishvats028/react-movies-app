1302f5e3b4832482d840aeac842e41f8651ebdfa
ashishvats028 <ashishvats028@gmail.com>
2021-06-07 00:17:17 +0530

    Login Button

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Home from './screens/Home/Home';
 
 ReactDOM.render(
-  <div></div>,
+  <Home/>,
   document.getElementById('root')
 );
 
